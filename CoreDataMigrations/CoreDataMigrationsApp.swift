f07263f1ae2acacc5bc44ef1948ba581d578ba71
Karina Gurachevskaya <k.gurachevskaya@pixelplex.io>
2022-09-21 16:29:02 +0200

    Project init

---
+++ CoreDataMigrations/CoreDataMigrationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataMigrationsApp.swift
+//  CoreDataMigrations
+//
+//  Created by Karina gurachevskaya on 21.09.22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataMigrationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
